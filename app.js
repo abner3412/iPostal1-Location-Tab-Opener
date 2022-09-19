5940cbba438cc3ceb6cf9a72d4eee71505852e96
[Joseph Jimenez] <93180479+abner3412@users.noreply.github.com>
2022-09-19 16:51:08 -0500

    added tab amount option

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const dotenv = require('dotenv').config();
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const inquirer = require('inquirer');
 
-(async () => {
+const questions = [
+  {
+    type: 'input',
+    name: 'Amount',
+    message: "Amount of tabs to open?",
+  },
+];
+
+inquirer.prompt(questions).then(answers => {
+
+  let locations = answers.Amount;
+  console.log(locations);
+
+  (async () => {
     puppeteer.use(StealthPlugin())
-
-    //while (timesCheckedOut < timesToCheckout){
       const browser = await puppeteer.launch({headless:false, args: ['--window-size=1920,1080'] 
     });
-    let locations = 1;
-    for (let i = 0; i < locations; i++){
 
+    for (let i = 0; i < locations; i++){
+     
     
       const page = await browser.newPage();
       await page.setDefaultNavigationTimeout(0);
@@ -21,7 +33,7 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
         deviceScaleFactor: 1,
       })
       
-      
+      await page.waitForSelector('input[id="username2"]', { timeout: 0 })
       await page.type('input[id="username2"]', process.env.userName)
       await page.type('input[id="password"]', process.env.password)
       await page.click('button[class="BB_buttonLogin"]')
@@ -32,10 +44,10 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
       await page.waitForSelector('#state_id', { timeout: 0 })
       await page.select('#state_id', value = '57')
       await page.waitForSelector('#cities', { timeout: 0 })
-      await page.select('#cities', value = 'Fort Worth')
+      await page.select('#cities', value = 'All')
       await page.waitForTimeout(5000)
       const locationValue = await page.$eval('#total-results-text', ele => ele.textContent);
-      locations = `${locationValue.substring(0, 2)}`;
+      //locations = `${locationValue.substring(0, 2)}`;
     }
-    
-})();    
+
+  })();});
